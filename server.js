7dc66915a735f7e74c1c0307e803515625f2cd84
k v sanjay raju <searchinnovative@gmail.com>
2024-11-22 23:26:30 +0530

    Update server.js to increase MongoDB connection timeout

--- server.js
+++ server.js
@@ -22,9 +22,11 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
 })
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error('MongoDB connection failed:', err));
+
 
 // Root Route
 app.get('/', (req, res) => {
