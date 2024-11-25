2c482947064d3c8b6fa0d0eb39c8b2d85127a8f7
kvsanjay123 <searchinnovative@gmail.com>
2024-11-25 05:33:48 -0800

    Update in mongoDB

--- frontend/src/Components/seedQuestion.js
+++ frontend/src/Components/seedQuestion.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "../App.css";
+
 
 const SeedQuestion = () => {
   const [questions, setQuestions] = useState([]);
