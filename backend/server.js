61840e4d6d0cc3b30a4674b4dfbc9d2ae27fbdc4
k v sanjay raju <searchinnovative@gmail.com>
2024-11-25 12:30:01 +0530

    dotenv line is added in top

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+
 
 // Initialize Express
 const app = express();
